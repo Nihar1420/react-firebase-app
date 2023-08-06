ffb5b1ed0e1452e337021aa2df6ab8b5a412ce81
Nihar1420 <niharranjanhota863@gmail.com>
2023-08-06 18:33:07 +0530

    Read executed

--- src/App.js
+++ src/App.js
@@ -1,8 +1,30 @@
-
+import { useEffect, useState } from "react";
+import { db } from "./firebaseConfig";
+import { collection, getDocs } from 'firebase/firestore';
 function App() {
+  const [users, setUsers] = useState([]);
+  const userCollectionRef = collection(db, 'users');
+  const getUsers = async () => {
+    const data = await getDocs(userCollectionRef);
+    setUsers(data.docs.map((doc) => ({
+      ...doc.data(), id: doc.id
+    })));
+  }
+  useEffect(() => {
+    getUsers();
+  }, [users.length]);
   return (
     <div className="App">
-      Hello
+      {
+        users.map((user) => {
+          return (
+            <div key={user.id}>
+              <h2>{user.name}</h2>
+              <h4>{user.age}</h4>
+            </div>
+          )
+        })
+      }
     </div>
   );
 }
--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,14 +1,24 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from '@firebase/firestore'
 
+// const firebaseConfig = {
+//     apiKey: process.env.local.REACT_API_KEY,
+//     authDomain: process.env.local.REACT_AUTH_DOMAIN,
+//     projectId: process.env.local.REACT_PROJECT_ID,
+//     storageBucket: process.env.local.REACT_STOARGE_BUCKET,
+//     messagingSenderId: process.env.local.REACT_MESSAGING_SENDER_ID,
+//     appId: process.env.local.REACT_APP_ID,
+//     measurementId: process.env.local.REACT_MEASUREMENT_ID
+// };
+
 const firebaseConfig = {
-    apiKey: process.env.REACT_API_KEY,
-    authDomain: process.env.REACT_AUTH_DOMAIN,
-    projectId: process.env.REACT_PROJECT_ID,
-    storageBucket: process.env.REACT_STOARGE_BUCKET,
-    messagingSenderId: process.env.REACT_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_ID,
-    measurementId: process.env.REACT_MEASUREMENT_ID
+    apiKey: "AIzaSyBqKw_-devSf5bbwdzTrZJ1dQlB3M7Qdz8",
+    authDomain: "react-firebase-app-71a00.firebaseapp.com",
+    projectId: "react-firebase-app-71a00",
+    storageBucket: "react-firebase-app-71a00.appspot.com",
+    messagingSenderId: "400911500791",
+    appId: "1:400911500791:web:2520b571e121d23a5c1af2",
+    measurementId: "G-RN4PT464EK"
 };
 
 const app = initializeApp(firebaseConfig);
